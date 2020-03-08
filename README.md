b7f81d0cd14ff458ab29c4eebace46f226e7e316
Bo Christian Skjøtt <bo@skjoett.com>
2020-03-08 22:38:42 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # RaspberryPi4
-Raspberry Pi 4 projects
+This repo contains Raspberry Pi 4 projects written in C# using .NET Core.
+
+| Folder | Content |
+|--------|---------|
+| led-flasher | Simple program that flashes an LED connected between GND (pin 9) and GPIO 17 (pin 11) |
+
+## Links
+* https://darenmay.com/blog/net-core-and-gpio-on-the-raspberry-pi---leds-and-gpio/
+* https://www.raspberrypi.org/documentation/usage/gpio/
+* https://www.hanselman.com/blog/InstallingTheNETCore2xSDKOnARaspberryPiAndBlinkingAnLEDWithSystemDeviceGpio.aspx
---
+++ led-flasher/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Device.Gpio;
+
+namespace LedFlasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pin = 17;
+            int lightTimeInMilliseconds = 1000;
+            int dimTimeInMilliseconds = 500;
+            
+            Console.WriteLine($"Let's blink an LED!");
+
+            using (GpioController controller = new GpioController())
+            {
+                controller.OpenPin(pin, PinMode.Output);
+                Console.WriteLine($"GPIO pin enabled for use: {pin}");
+
+                Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs eventArgs) =>
+                {
+                    controller.Dispose();
+                };
+
+                while (true)
+                {
+                    Console.WriteLine($"Light for {lightTimeInMilliseconds}ms");
+                    controller.Write(pin, PinValue.High);
+                    Thread.Sleep(lightTimeInMilliseconds);
+
+                    Console.WriteLine($"Dim for {dimTimeInMilliseconds}ms");
+                    controller.Write(pin, PinValue.Low);
+                    Thread.Sleep(dimTimeInMilliseconds);
+                }
+            }
+        }
+    }
+}
