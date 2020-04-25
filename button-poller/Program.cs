1db99a3dbcab13c040f362d93811eb790cbdaf8f
Bo Christian Skjøtt <bo@skjoett.com>
2020-04-25 22:12:15 +0200

    Added or updated

--- button-poller/Program.cs
+++ button-poller/Program.cs
@@ -7,25 +7,29 @@ namespace ButtonPoller
     //
     // This program requires that a normally-open push button is connected between GPIO 22 (pin 15) and 3.3V on pin 17.
     // with a 470 Ohms resistor in series.
-    // It also requires that an LED is connected between GPIO 17 (pin 11) and GND (pin 9)
+    // It assumes that an LED is connected between GPIO 17 (pin 11) and GND (pin 9)
     // with a 390 Ohms resistor in series.
-    //
+    // It assumes that a piezo buzzer is connected between GPIO 26 (pin 37) and GND (pin 39).
     class Program
     {
         static void Main(string[] args)
         {
             int ledPin = 17;
             int buttonPin = 22;
+            int buzzerPin = 26;
             
-            Console.WriteLine("Press the button to turn on the LED");
+            Console.WriteLine("Press the button to turn on the LED and buzz");
 
             using (GpioController controller = new GpioController())
             {
                 controller.OpenPin(ledPin, PinMode.Output);
-                Console.WriteLine($"LED GPIO pin set to output mode: {ledPin}");
+                Console.WriteLine($"LED GPIO pin {ledPin} set to output mode");
 
                 controller.OpenPin(buttonPin, PinMode.InputPullDown);
-                Console.WriteLine($"Button GPIO pin set to input mode: {buttonPin}");
+                Console.WriteLine($"Button GPIO pin {buttonPin} set to input mode");
+
+                controller.OpenPin(buzzerPin, PinMode.Output);
+                Console.WriteLine($"Buzzer GPIO pin {buzzerPin} set to output mode");
 
                 Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs eventArgs) =>
                 {
@@ -36,13 +40,21 @@ namespace ButtonPoller
                 {
                     if (controller.Read(buttonPin) == PinValue.High)
                     {
-                        // Button is pressed, turn on LED
+                        // Button is pressed
+                        // Turn on LED
                         controller.Write(ledPin, PinValue.High);
+
+                        // Turn on buzzer
+                        controller.Write(buzzerPin, PinValue.High);
+
                     }
                     else
                     {
                         // Turn LED off
                         controller.Write(ledPin, PinValue.Low);
+
+                        // Turn buzzer off
+                        controller.Write(buzzerPin, PinValue.Low);
                     }
 
                     Thread.Sleep(200);
---
+++ google-assistant/README.md
@@ -0,0 +1,20 @@
+# Description
+
+Using Google Assistant on a Raspberry Pi 4.
+
+See: https://pimylifeup.com/raspberry-pi-google-assistant/
+
+## Device Registration
+Product name: Pi4 Google Assistant
+Device model ID: raspberry-37131-pi4-google-assistant-xks4im
+
+
+## Authentication
+Place the client_secret_xxx.json file in the folder where you're running the Assistant SDK.
+
+To copy over SSH to a remote device, run the following command from your current computer:
+
+```
+scp ~/Downloads/client_secret_xxx.json <username>@<device-ip-address>:</path/to/assistant-sdk/project>
+```
+
