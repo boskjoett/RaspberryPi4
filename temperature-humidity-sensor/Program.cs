f08a4370842bdb292499dac6953b162fe4a08869
Bo Christian Skjøtt <bo@skjoett.com>
2022-01-01 23:00:21 +0100

    Added DHT11 project

---
+++ temperature-humidity-sensor/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading;
+using System.Device.Gpio;
+
+namespace LedFlasher
+{
+    //
+    // This program assumes that a DHT11 temperature and humidity sensor
+    // has its data pin connected to GPIO pin 2.
+    //
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pin = 18;
+            int temperature;
+            int humidity;
+            
+            Console.WriteLine("Reading temperature and humidity");
+
+            using (GpioController controller = new GpioController())
+            {
+                // Set pin in output mode
+                controller.OpenPin(pin, PinMode.Output);
+
+                // Write start pulse by setting pin 
+                // high, then low for minimum 18 ms, then high.
+                controller.Write(pin, PinValue.High);
+                Thread.Sleep(100);
+
+                controller.Write(pin, PinValue.Low);
+                Thread.Sleep(50);
+                controller.Write(pin, PinValue.High);
+
+                // Set pin in input mode
+                controller.OpenPin(pin, PinMode.Input);
+
+                // After a start pulse the sensor sends a response pulse where input goes from high to low.
+                // Detect the transition from high to low.
+
+                int loopCounter = 0;
+
+                while (controller.Read(pin) == PinValue.High && loopCounter < 10000)
+                {
+                    loopCounter++;
+                }
+
+                if (loopCounter >= 10000)
+                {
+                    Console.WriteLine("Response high to low transition not detected");
+                    return;
+                }
+
+                loopCounter = 0;
+                while (controller.Read(pin) == PinValue.Low && loopCounter < 10000)
+                {
+                    loopCounter++;
+                }
+
+                if (loopCounter >= 10000)
+                {
+                    Console.WriteLine("Response low to high transition not detected");
+                    return;
+                }
+
+                // Read 5 bytes of data from sensor
+                // Each byte contains 8 bits.
+                // First two bytes contains the humidity.
+                // Next two bytes contains the temperature.
+                // 5th byte is a checksum of the previous 4 bytes. The checksum is the unsigned sum of the first 4 bytes.
+
+                byte[] data = new byte[5];
+
+                for (int bytesRead = 0; bytesRead < 5; bytesRead++)
+                {
+                    // Each data bit starts with a high to low transition that stays low for 54 microseconds
+                    loopCounter = 0;
+
+                    while (controller.Read(pin) == PinValue.High && loopCounter < 10000)
+                    {
+                        loopCounter++;
+                    }
+
+                    if (loopCounter >= 10000)
+                    {
+                        Console.WriteLine("Data high to low transition not detected");
+                        return;
+                    }
+
+                    // Detect low to high transition. The duration of the high state determines the bit value
+                    loopCounter = 0;
+
+                    while (controller.Read(pin) == PinValue.Low && loopCounter < 10000)
+                    {
+                        loopCounter++;
+                    }
+
+                    if (loopCounter >= 10000)
+                    {
+                        Console.WriteLine("Data low to high transition not detected");
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ temperature-humidity-sensor/README.md
@@ -0,0 +1,7 @@
+Connecting DHT11 to Raspberry Pi
+
+* https://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-the-raspberry-pi/
+
+Single-wire protocol
+
+* https://www.electronicwings.com/sensors-modules/dht11#:~:text=DHT11%20is%20a%20single%20wire,0%20to%2050%20%C2%B0C).
